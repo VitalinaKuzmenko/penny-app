5ce04fe6f8817d844e89b7e7503b803c52bfe6a5
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2025-12-06 23:37:01 +0000

    Improve eslint base rules

--- .eslintrc.base.js
+++ .eslintrc.base.js
@@ -5,14 +5,47 @@ module.exports = {
     node: true,
     browser: true,
   },
+
+  parser: '@typescript-eslint/parser',
+
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+
+  plugins: ['@typescript-eslint', 'import', 'unused-imports', 'prettier'],
+
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+
   rules: {
-    'prettier/prettier': ['error'],
-    'no-unused-vars': 'warn',
-    'no-console': 'off',
+    // --- Prettier ---
+    'prettier/prettier': 'error',
+
+    // --- Remove unused imports automatically ---
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+
+    // --- Import order ---
+    'import/order': [
+      'error',
+      {
+        'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'alphabetize': { order: 'asc', caseInsensitive: true },
+        'newlines-between': 'always',
+      },
+    ],
+
+    // --- General improvements ---
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    '@typescript-eslint/no-unused-vars': 'off', // replaced by unused-imports
   },
 };
