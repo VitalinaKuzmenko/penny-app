08d8fe3e42bab35252413c2fa83c66a57bef562d
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2025-12-07 13:56:53 +0000

    Add new favicon, remove next.js image, styles and text

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,11 +1,9 @@
-import * as path from 'path';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  turbopack: {
-    root: path.join(__dirname), // points to the Next.js app root
-  },
+  // turbopack: {
+  //   root: path.join(__dirname), // points to the Next.js app root
+  // },
 };
 
 module.exports = nextConfig;
