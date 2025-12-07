28d9d8f2618d71c0fcf461cdb7510f1e5218de52
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2025-12-07 14:27:47 +0000

    Add custom app colout to mui theme

--- ui/src/providers/ThemeProvider.tsx
+++ ui/src/providers/ThemeProvider.tsx
@@ -4,11 +4,41 @@ import { ThemeProvider as MuiThemeProvider, createTheme } from '@mui/material/st
 import CssBaseline from '@mui/material/CssBaseline';
 import { ReactNode } from 'react';
 
-const theme = createTheme({
+export const theme = createTheme({
   palette: {
     mode: 'light',
     primary: {
-      main: '#1976d2',
+      main: '#041646',
+      light: '#263476',
+      dark: '#03123d',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#00B1C2',
+      light: '#26bfce',
+      dark: '#009eaf',
+      contrastText: '#ffffff',
+    },
+    success: {
+      main: '#10B981',
+    },
+    warning: {
+      main: '#F59E0B',
+    },
+    error: {
+      main: '#EF4444',
+    },
+    grey: {
+      50: '#f8f9fa',
+      100: '#f1f3f5',
+      200: '#e9ecef',
+      300: '#dee2e6',
+      400: '#ced4da',
+      500: '#adb5bd',
+      600: '#6c757d',
+      700: '#495057',
+      800: '#343a40',
+      900: '#212529',
     },
   },
 });
