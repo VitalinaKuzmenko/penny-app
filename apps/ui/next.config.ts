1cfcbcebfbbcfb6a28d1694deaf04aebda71a1c7
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2025-12-25 20:44:44 +0000

    comment turbopack root in the next.config.ts

--- apps/ui/next.config.ts
+++ apps/ui/next.config.ts
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  turbopack: {
-    root: __dirname, // or './apps/ui'
-  },
+  // turbopack: {
+  //   root: __dirname, // or './apps/ui'
+  // },
 };
 
 module.exports = nextConfig;
