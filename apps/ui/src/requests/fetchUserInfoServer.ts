286f260824cb76f342bfd640ce9a7259da11d8e4
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2025-12-26 14:29:08 +0000

    Add debbug logs

--- apps/ui/src/requests/fetchUserInfoServer.ts
+++ apps/ui/src/requests/fetchUserInfoServer.ts
@@ -2,27 +2,30 @@ import { cookies } from 'next/headers';
 import { UserInfo } from 'schemas';
 
 export const fetchUserInfoServer = async (): Promise<UserInfo | null> => {
-  let user: UserInfo | null = null;
-  try {
-    const cookieStore = cookies();
-    const accessToken = (await cookieStore).get('access_token')?.value;
+  const cookieStore = cookies();
+  const accessToken = (await cookieStore).get('access_token')?.value;
 
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/profile`,
-      {
-        cache: 'no-store',
-        headers: {
-          Cookie: `access_token=${accessToken}`,
-        },
-      },
+  console.log('fetchUserInfoServer: accessToken =', accessToken);
+
+  if (!accessToken) return null;
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/profile`,
+    {
+      headers: { Cookie: `access_token=${accessToken}` },
+      cache: 'no-store',
+    },
+  );
+
+  if (!res.ok) {
+    console.log(
+      'fetchUserInfoServer: failed to fetch profile',
+      await res.text(),
     );
-
-    if (res.ok) {
-      user = await res.json();
-    }
-  } catch (err) {
-    console.error('Failed to fetch profile', err);
+    return null;
   }
 
+  const user = await res.json();
+  console.log('fetchUserInfoServer: user =', user);
   return user;
 };
