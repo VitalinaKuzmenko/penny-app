191a216c9e8f8eae451439a478cee5b37dcea312
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2026-01-02 14:02:06 +0000

    Create template of upload csv page

---
+++ apps/ui/src/app/[lang]/upload-csv/page.tsx
@@ -0,0 +1,22 @@
+import { Container } from '@mui/material';
+import { LanguageType } from '@/utils/interfaces';
+import { getDictionary } from '@/utils/getDictionary';
+import ProfileClient from '@/components/ProfileClient/ProfileClient';
+
+export const dynamic = 'force-dynamic';
+
+export default async function UploadCsvPage({
+  params,
+}: {
+  params: Promise<{ lang: LanguageType }>;
+}) {
+  const { lang } = await params;
+  const dict = await getDictionary(lang);
+  const profilePageText = dict.UPLOAD_CSV_PAGE;
+
+  return (
+    <Container maxWidth="sm">
+      <ProfileClient profilePageText={profilePageText} />
+    </Container>
+  );
+}
